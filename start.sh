1a1a7a86683fdce3fda00e303e505162a1c5358f
Danilo Sicari <denadai.sicari@gmail.com>
2020-01-25 21:18:32 -0300

    Modified start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/home/pi/mjpg_streamer/streamer.sh start
-cd /home/pi/timelapse && python3 main.py
+/home/pi/mjpg_streamer/streamer.sh start &
+cd /home/pi/timelapse && python3 main.py noconsole &
